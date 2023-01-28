5637aab370b375bf660c4ef84b7484a232fc8272
Javapisk <giagea26@gmail.com>
2023-01-28 13:28:11 -0300

    Тернарные опреаторы

--- task2/src/Main.java
+++ task2/src/Main.java
@@ -19,16 +19,19 @@ public class Main {
         //todo дописать логику программы ниже.
 
 
+        greatest = first > second && first > third ? first : second > third ? second : third;
+        smallest = first < second && first < third ? first : second < third ? second : third;
+        if ((first < second && first > third) || (first > second && first < third)){
+            average = first;
+        } else if ((second < first && second > third) || (second > first && second < third)) {
+            average = second;
+        } else {
+                average = third;
+            }
 
 
-
-
-
-        //todo
-
-
-        System.out.println("Наибольший вес: " + 10);
-        System.out.println("Средний вес: " + 9);
-        System.out.println("Наименьший вес: " + 1);
+            System.out.println("Наибольший вес: " + greatest);
+            System.out.println("Средний вес: " + average);
+            System.out.println("Наименьший вес: " + smallest);
+        }
     }
-}
